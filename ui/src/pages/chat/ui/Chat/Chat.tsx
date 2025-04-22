20059285d58a391e1becbec754d59b1a8b89198e
Asker Khuzhokov <a7kerkh@yandex.ru>
2025-04-22 12:12:35 +0300

    feat(ui): prettified

--- ui/src/pages/chat/ui/Chat/Chat.tsx
+++ ui/src/pages/chat/ui/Chat/Chat.tsx
@@ -2,22 +2,38 @@ import * as React from 'react';
 
 import transform from '@diplodoc/transform';
 import {YfmStaticView} from '@gravity-ui/markdown-editor';
+import {Card, TextInput} from '@gravity-ui/uikit';
 
 import {useChat} from '../../api/chat';
+
+import styles from './Chat.module.css';
 
 export function Chat() {
     const [controller] = React.useState(() => new AbortController());
     const [messages, mutate] = useChat({signal: controller.signal});
     const html = React.useMemo(() => transform(messages.join('')).result.html, [messages]);
 
-    const onClick = () => {
-        mutate({message: 'Tell me about Elon Musk in 10 sentences'});
+    const onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const prompt = formData.get('prompt')?.toString().trim();
+
+        if (!prompt) {
+            return;
+        }
+
+        event.currentTarget.reset();
+        mutate({message: prompt});
     };
 
     return (
-        <>
-            <button onClick={onClick}>click</button>
-            <YfmStaticView html={html} noListReset />
-        </>
+        <form className={styles.form} onSubmit={onSubmit}>
+            <TextInput name="prompt" placeholder="Write something..." />
+            {html ? (
+                <Card width="100%" spacing={{p: 6}} view="filled">
+                    <YfmStaticView html={html} noListReset />
+                </Card>
+            ) : null}
+        </form>
     );
 }
